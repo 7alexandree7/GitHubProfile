6f6908881a419118aee825c656fe3716240fb13b
Seu Nome <seuemail@dominio.com>
2024-12-06 07:17:19 -0300

    add lib shadcnUi

---
+++ github_profile/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <h3>GitHubProfile</h3>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
