7d4faeddbb046fe0b794724c90f9df15d7938c0e
Seu Nome <seuemail@dominio.com>
2024-12-06 08:30:30 -0300

    1 configuração no NavBar

--- github_profile/src/app/page.tsx
+++ github_profile/src/app/page.tsx
@@ -1,9 +1,10 @@
+import NavBar from "@/components/NavBar/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      teste
-    </div>
+    <>
+    <NavBar />
+    </>
   );
 }
--- github_profile/src/components/NavBar/NavBar.tsx
+++ github_profile/src/components/NavBar/NavBar.tsx
@@ -1,14 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Input } from "../ui/input";
+import { BellRing, Heart, Mail } from "lucide-react";
+import PulsatingButton from "../ui/pulsating-button";
+import AvatarCircles from "../ui/avatar-circles";
+import Image from "next/image";
+import TypingAnimation from "../ui/typing-animation";
+//border-[1px] border-solid border-red-500
 
- const NavBar = () => {
+const avatarData = [
+  {
+    imageUrl: "https://github.com/7alexandree7.png",
+    profileUrl: "https://github.com/7alexandree7.png",
+  },
+];
+
+const NavBar = () => {
   return (
-    <div>
-      <div>
-        <h3>GitHubProfile</h3>
+    <div className="flex w-full justify-between items-center pt-6 pl-12 pr-12">
+      <div className="flex items-center gap-4 w-[280px]">
+        <Image 
+              src="/logo.png"
+              width={36}
+              height={36}
+              alt="Picture of the author"
+        />
+        <TypingAnimation  text="GitHub Profile"/>
       </div>
-      <div>
-        
+
+      <div className="w-[400px]">
+        <Input placeholder="Search for a user..."/>
+      </div>
+
+      <div className="flex gap-4 items-center">
+        <div className="flex gap-4">
+          <Mail />
+          <BellRing />
+        </div>
+        <div>
+          <PulsatingButton />
+        </div>
+        <div>
+          <AvatarCircles avatarUrls={avatarData} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default NavBar;
