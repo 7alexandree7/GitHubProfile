01e92e049973e4358b6ff17b5fc5193112bbd3e4
alexandre <alexandre020602@gmail.com>
2024-12-06 22:10:09 -0300

    correção

--- github_profile/src/components/NavBar/NavBar.tsx
+++ github_profile/src/components/NavBar/NavBar.tsx
@@ -1,20 +1,15 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
 import FirstChildLeft from "../Header-firstChildLeft/FirstChildLeft";
 import HeaderSearch from "../HeaderSearch/HeaderSearch";
 import LastChildRight from "../Header-LastChildRight/LastChildRight";
-//border-[1px] border-solid border-red-500
-
-const avatarData = [
-  {
-    imageUrl: "https://github.com/7alexandree7.png",
-    profileUrl: "https://github.com/7alexandree7.png",
-  },
-];
 
 const NavBar = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 w-full h-[180px] bg-gradient-to-r from-[#A2D2FF] via-[#CDB4DB] to-[#FFC8DD] z-0"></div>
+      <div className="absolute top-0 left-0 w-full h-[180px] z-0 overflow-hidden">
+      </div>
 
       <div className="relative z-10 flex w-full justify-between items-center pt-6 pl-6 pr-6">
         <FirstChildLeft />
